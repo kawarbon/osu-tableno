91dd21dabefa3b900c952939c81522020bf9787a
kawarbon <182037181+kawarbon@users.noreply.github.com>
2024-11-01 16:30:04 +0200

    hung's fontawesome is broken

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,13 +9,11 @@ export default function App(
             <Head>
                 <link rel="stylesheet" href="/styles.css" />
 
-                {/* https://fa.hung1001.com/ */}
                 <link
-                    href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
                     rel="stylesheet"
                     type="text/css"
                 />
-
                 <link
                     href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
                     rel="stylesheet"
